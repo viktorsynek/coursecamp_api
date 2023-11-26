5c64a4c16dffffd985a7fc2f62d8560ad17dcd12
viktorsynek <ktor.syn@gmail.com>
2023-11-26 13:00:02 +0100

    use errorHandler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 
 const connectDB = require('./config/db');
 dotenv.config({path: './config/config.env'});
@@ -12,12 +13,15 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+app.use(express.json());
+
 if(process.env.NODE_ENV === 'development')
 {
     app.use(morgan('dev'));
 }
 
 app.use('/api/bootcamps', bootcamps);
+app.use( errorHandler);
 
 
 const server = app.listen(process.env.PORT, console.log(`Server is running in ${process.env.NODE_ENV} on ${process.env.PORT}`.cyan.bold));
