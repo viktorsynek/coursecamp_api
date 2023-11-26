122204c898883e258f5d67a299b5769215939e81
viktorsynek <ktor.syn@gmail.com>
2023-11-26 13:01:12 +0100

    add name slugify and address geocoder

--- modules/Bootcamp.js
+++ modules/Bootcamp.js
@@ -46,7 +46,6 @@ const BootcampSchema = new mongoose.Schema(
       },
       coordinates: {
         type: [Number],
-        required: true,
         index: '2dsphere'
       },
       formattedAddress: String,
@@ -99,5 +98,33 @@ const BootcampSchema = new mongoose.Schema(
       default: Date.now
     }
 });
+
+
+// Create slug 
+BootcampSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  }; 
+
+  this.address = undefined;
+  next();
+});
+
+
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
